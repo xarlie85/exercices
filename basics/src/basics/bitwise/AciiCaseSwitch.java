ed7792b5b9dbc5d6e54fcb67ba4fa035c3d0b8b2
Carlos <xarlie85@gmail.com>
2019-02-28 17:43:35 +0100

    md formatting

--- basics/src/basics/bitwise/AciiCaseSwitch.java
+++ basics/src/basics/bitwise/AciiCaseSwitch.java
@@ -13,8 +13,14 @@ import java.util.stream.Stream;
  */
 public class AciiCaseSwitch {
 	
+	/**
+	 * These start and end tags are for keeping spaces in the markdown output file
+	 */
+	private static final String startKeepingSpaces = "<pre>";
+	private static final String endKeepingSpaces = "</pre>";
+	
 	private static PrintWriter readme = null;
-
+	
 	private static String bin(char c) {
 		return Integer.toBinaryString(c) + "(" + (int) c + ") --> [" + c + "]";
 	}
@@ -42,12 +48,12 @@ public class AciiCaseSwitch {
 		Character mask = '_';
 		char r = (char) (c & mask);
 		readme.write("[" + c + "] To upper case:  \n");
-		readme.write("    " + bin(c) + "  \n& " + bin(mask) + "  \n  " + bin(r) + "  \n\n");
+		readme.write("  " + bin(c) + "  \n& " + bin(mask) + "  \n  " + bin(r) + "  \n\n");
 	}
 
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 		readme = new PrintWriter("src/basics/bitwise/README.md", "UTF-8");
-
+		readme.write(startKeepingSpaces);
 		//iteration over all the ascii uppper case chars
 		Stream.iterate(65, (d) -> d + 1).limit(26).forEach((d) -> toLowerCase((char) d.intValue()));
 		
@@ -58,6 +64,7 @@ public class AciiCaseSwitch {
 		char c = 'ñ';
 		toUpperCase(c); // not working properly!
 		
+		readme.write(endKeepingSpaces);
 		readme.close();
 	}
 
